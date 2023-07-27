e2bf293d3f6b931aa798c0fd1887a33f1a5763e2
Yoga Vredizon <yogavredizon7@gmail.com>
2023-07-27 19:39:02 +0700

    Edit API

--- api/hello.go
+++ api/hello.go
@@ -1,5 +1,12 @@
 package api
 
-func SayHello() string {
-	return "Hello, World"
+import (
+	"fmt"
+
+	m "github.com/yogavredizon/go-modules/models"
+)
+
+func SayHello() {
+	person := m.Person{Name: "Yoga"}
+	fmt.Println(person.GetName())
 }
