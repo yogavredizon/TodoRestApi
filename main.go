02eb2e99a363ecde979dffde30360c0b4de4f69b
Yoga Vredizon <yogavredizon@gmail.com>
2023-10-12 22:54:01 +0700

    Updating error message

--- main.go
+++ main.go
@@ -6,37 +6,37 @@ import (
 	"os"
 	"strconv"
 
-	rp "github.com/yogavredizon/todolist/pkg/repository"
+	repo "github.com/yogavredizon/todolist/pkg/repository"
 )
 
 func main() {
 
 	run := 1
-	var todo rp.Todo
+	var todo repo.Todo
 	scanner := bufio.NewScanner(os.Stdin)
 
 	for run > 0 {
 		fmt.Print("\nTodolist\n")
 		todo.ShowTodo()
 
-		fmt.Println("\nMenu Todolist")
-		fmt.Print(" 1. Tambah Todo \n 2. Update Todo \n 3. Update Time \n 4. Hapus Todo \n 5. Keluar \n")
+		fmt.Println("\nTodolist Menu")
+		fmt.Print(" 1. Add Todo \n 2. Update Todo \n 3. Update Time \n 4. Remove Todo \n 5. Exit \n")
 
 		// Get input from terminal dan convert into integer
-		fmt.Print("Pilih Menu : ")
+		fmt.Print("Choose Menu : ")
 		scanner.Scan()
 		num, _ := strconv.Atoi(scanner.Text())
 
 		switch num {
 		case 1:
-			fmt.Println("Tambah Todo : ")
+			fmt.Println("Add Todo : ")
 
 			fmt.Print("Todo : ")
 			scanner.Scan()
 			scanner.Text()
 			task := scanner.Text()
 
-			fmt.Print("Waktu : ")
+			fmt.Print("Time : ")
 			scanner.Scan()
 			scanner.Text()
 			time := scanner.Text()
@@ -46,13 +46,13 @@ func main() {
 				fmt.Printf("\nError : %v \n", err)
 			}
 		case 2:
-			fmt.Println("Update Todo dari Id")
-			fmt.Print("Masukan ID : ")
+			fmt.Println("Update Todo from ID")
+			fmt.Print("Choose ID : ")
 			scanner.Scan()
 			scanner.Text()
 			id, _ := strconv.Atoi(scanner.Text())
 
-			fmt.Print("Masukan Todo : ")
+			fmt.Print("Fill the Todo : ")
 			scanner.Scan()
 			scanner.Text()
 			task := scanner.Text()
@@ -65,8 +65,8 @@ func main() {
 			}
 
 		case 3:
-			fmt.Println("Update Waktu dari Id")
-			fmt.Print("Masukan ID : ")
+			fmt.Println("Update time from ID")
+			fmt.Print("Choose ID : ")
 			scanner.Scan()
 			scanner.Text()
 			id, _ := strconv.Atoi(scanner.Text())
@@ -97,7 +97,7 @@ func main() {
 		case 5:
 			run = 0
 		default:
-			fmt.Println("Pilihan tidak ada")
+			fmt.Println("Fill with number of menu")
 		}
 	}
 
